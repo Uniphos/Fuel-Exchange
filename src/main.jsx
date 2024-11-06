bc665612b753ae5a2a184ff83d7bd31b7c71e2e4
UniPhos <victor.ribas6@gmail.com>
2024-11-06 14:12:19 -0500

    deploy yml

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,10 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <BrowserRouter basename={"/Fuel-Exchange/"}>
-      <Routes>
-        <Route path="*" element={<App />} />
-      </Routes>
+      <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </StrictMode>,
 )
